fb1a3eb861eaf79438cd0afdfab438e378e92366
ArjunDahal99 <dahalarjun409@gmail.com>
2024-04-28 08:49:53 +0545

    Add project files.

---
+++ lab2/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using lab2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab2.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student { Id = 1, Name = "Arjun", Address = "Kapan", Age = 20 },
+                new Student { Id = 2, Name = "Aayush", Address = "Patan", Age = 18 },
+                new Student { Id = 3, Name = "Anish", Address = "Bhakatapur", Age = 34 }
+            };
+
+
+            return View( students);
+        }
+    }
+
+}
---
+++ lab2/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace lab2.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+
+    }
+}
